fce06db5d522bab2e0790f454b41f75f66c92de1
leonaardomuller <leonaardo.muller@gmail.com>
2021-05-06 12:07:14 -0300

    :art:Final Module

--- src/pages/Profile/index.tsx
+++ src/pages/Profile/index.tsx
@@ -1,9 +1,9 @@
-import React, { useCallback, useRef } from 'react';
-import { FiMail, FiLock, FiUser } from 'react-icons/fi';
+import React, { ChangeEvent, useCallback, useRef } from 'react';
+import { FiMail, FiLock, FiUser, FiCamera, FiArrowLeft } from 'react-icons/fi';
 import { FormHandles } from '@unform/core';
 import { Form } from '@unform/web';
 import * as Yup from 'yup';
-import { useHistory } from 'react-router-dom';
+import { Link, useHistory } from 'react-router-dom';
 import getValidationErrors from '../../utils/getValidationErrors';
 
 import api from '../../services/api';
@@ -13,18 +13,24 @@ import { useToast } from '../../hooks/toast';
 import Button from '../../components/Button';
 import Input from '../../components/Input';
 
-import { Container, Content } from './styles';
+import { Container, Content, AvatarInput } from './styles';
+import { useAuth } from '../../hooks/auth';
 
 interface ProfileFormData {
   name: string;
   email: string;
+  old_password: string;
   password: string;
+  password_confirmation: string;
 }
 
 const Profile: React.FC = () => {
   const formRef = useRef<FormHandles>(null);
   const { addToast } = useToast();
   const history = useHistory();
+
+  const { user, updateUser } = useAuth();
+
   const handleSubmit = useCallback(
     async (data: ProfileFormData) => {
       try {
@@ -35,23 +41,54 @@ const Profile: React.FC = () => {
           email: Yup.string()
             .required('E-mail obrigatório')
             .email('Digite um e-mail válido'),
-          password: Yup.string()
-            .min(6, 'No mínimo 6 dígitos')
-            .max(12, 'No máximo 12 digitos'),
+          old_password: Yup.string(),
+          password: Yup.string().when('old_password', {
+            is: val => !!val.length,
+            then: Yup.string().required('Campo obrigatório'),
+            otherwise: Yup.string(),
+          }),
+          password_confirmation: Yup.string()
+            .when('old_password', {
+              is: val => !!val.length,
+              then: Yup.string().required('Campo obrigatório'),
+              otherwise: Yup.string(),
+            })
+            .oneOf([Yup.ref('password')], 'As senhas devem bater'),
         });
 
         await schema.validate(data, {
           abortEarly: false,
         });
 
-        await api.post('/users', data);
+        await api.put('/profile', data);
 
-        history.push('/');
+        const {
+          name,
+          email,
+          old_password,
+          password,
+          password_confirmation,
+        } = data;
+
+        const formData = {
+          name,
+          email,
+          ...(old_password
+            ? { old_password, password, password_confirmation }
+            : {}),
+        };
+
+        const response = await api.put('/profile', data);
+
+        updateUser(response.data);
+
+        history.push('/dashboard');
 
         addToast({
           type: 'success',
-          title: 'Cadastro realizado com sucesso !',
-          description: 'Você já pode fazer seu logon',
+          title: 'Perfil atualizado!',
+          description:
+            'Suas informações do perfil foram atualizadas com sucesso!',
         });
       } catch (err) {
         if (err instanceof Yup.ValidationError) {
@@ -64,25 +101,71 @@ const Profile: React.FC = () => {
 
         addToast({
           type: 'error',
-          title: 'Erro no cadastro',
+          title: 'Erro na atualização do perfil',
           description:
-            'Ocorreu um erro ao fazer o cadastro, verifique as informações e tente novamente.',
+            'Ocorreu um erro na atualização do perfil, verifique as informações e tente novamente.',
         });
       }
     },
     [addToast, history],
   );
 
+  const handleAvatarChange = useCallback(
+    (e: ChangeEvent<HTMLInputElement>) => {
+      if (e.target.files) {
+        const data = new FormData();
+
+        data.append('avatar', e.target.files[0]);
+
+        api.patch('/users/avatar', data).then(response => {
+          updateUser(response.data);
+
+          addToast({
+            type: 'success',
+            title: 'Avatar atualizado!',
+          });
+        });
+      }
+    },
+    [addToast, updateUser],
+  );
+
   return (
     <Container>
+      <header>
+        <div>
+          <Link to="/dashboard">
+            <FiArrowLeft />
+          </Link>
+        </div>
+      </header>
       <Content>
-        <Form ref={formRef} onSubmit={handleSubmit}>
+        <Form
+          ref={formRef}
+          // initialData={{
+          //   name: user.name,
+          //   email: user.email,
+          // }}
+          onSubmit={handleSubmit}
+        >
+          <AvatarInput>
+            <img
+              src="https://avatars1.githubusercontent.com/u/60147880?v=4"
+              alt="Usename"
+            />
+            {/* <img src={user.avatar_url} alt={user.name} /> */}
+            <label htmlFor="avatar">
+              <FiCamera />
+              <input type="file" id="avatar" onChange={handleAvatarChange} />
+            </label>
+          </AvatarInput>
           <h1>Meu perfil</h1>
 
           <Input name="name" icon={FiUser} placeholder="Nome" />
           <Input name="email" icon={FiMail} placeholder="E-mail" />
 
           <Input
+            containerStyle={{ marginTop: 24 }}
             name="old_password"
             icon={FiLock}
             type="password"
--- src/pages/Profile/styles.ts
+++ src/pages/Profile/styles.ts
@@ -2,10 +2,25 @@ import styled from 'styled-components';
 import { shade } from 'polished';
 
 export const Container = styled.div`
-  height: 100vh;
+  > header {
+    height: 144px;
+    background: #28262e;
 
-  display: flex;
-  align-items: stretch;
+    display: flex;
+    align-items: center;
+
+    div {
+      width: 100%;
+      max-width: 1120px;
+      margin: 0 auto;
+    }
+
+    svg {
+      color: #999591;
+      width: 30px;
+      height: 30px;
+    }
+  }
 `;
 
 export const Content = styled.div`
@@ -13,7 +28,7 @@ export const Content = styled.div`
   flex-direction: column;
   align-items: center;
   justify-content: center;
-  margin: 0 auto;
+  margin: -176px auto;
 
   width: 100%;
 
@@ -21,7 +36,8 @@ export const Content = styled.div`
     margin: 80px;
     width: 340px;
     text-align: center;
-
+    display: flex;
+    flex-direction: column;
     h1 {
       margin-bottom: 24px;
       font-size: 20px;
@@ -42,6 +58,49 @@ export const Content = styled.div`
 
     input[name='old_password'] {
       margin-top: 24px;
+    }
+  }
+`;
+
+export const AvatarInput = styled.div`
+  margin-bottom: 32px;
+  position: relative;
+  align-self: center;
+
+  img {
+    width: 186px;
+    height: 186px;
+    border-radius: 50%;
+  }
+
+  label {
+    position: absolute;
+    width: 48px;
+    height: 48px;
+    background: #cb0d16;
+    border-radius: 50%;
+    right: 0;
+    bottom: 0;
+    border: 0;
+    cursor: pointer;
+    transition: background-color 0.2s;
+
+    display: flex;
+    align-items: center;
+    justify-content: center;
+
+    input {
+      display: none;
+    }
+
+    svg {
+      width: 20px;
+      height: 20px;
+      color: #312e38;
+    }
+
+    &:hover {
+      background: ${shade(0.2, '#cb0d16')};
     }
   }
 `;
